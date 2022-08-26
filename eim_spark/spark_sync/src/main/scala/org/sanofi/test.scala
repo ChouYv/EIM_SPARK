9dfad1219fbe0348ce7d14c5414490d67d9e6d07
zhouyahui <zyhsszx@163.com>
2022-08-26 02:28:39 -0400

    scripts
    Signed-off-by: zhouyahui <zyhsszx@163.com>

--- eim_spark/spark_sync/src/main/scala/org/sanofi/test.scala
+++ eim_spark/spark_sync/src/main/scala/org/sanofi/test.scala
@@ -1,24 +1,66 @@
 package org.sanofi
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 
 object test {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
-      .appName("hive test")
+      .appName("generateScripts")
       .master("yarn")
-      .enableHiveSupport()
       .getOrCreate()
-    import spark.implicits._
-    import spark.sql
-    sql("show databases;").show()
-    sql("show tables").show()
-    sql("create table if not exists test001(id int,name string)")
-    sql("insert into table test001 values(1,'zhouyahui')")
-    sql("insert into table test001 values(2,'zhouyahui2')")
-    sql("show create table test001 ").show(100)
+    spark.sparkContext.setLogLevel("WARN")
+    val inputFileName = "MD_CL_CONTENT"
+    val BDMPPostgresUrl = "jdbc:postgresql://localhost:55432/postgres"
+    val BDMPPostgresUser = "postgres"
+    val BDMPPostgresPwd = "rootroot"
+
+
+    val connOptionsMap =
+      Map("url" -> BDMPPostgresUrl,
+        "user" -> BDMPPostgresUser,
+        "password" -> BDMPPostgresPwd)
+
+    val bdmpInterfaceDf = getPostgresDf(spark, connOptionsMap, "app_upt.bdmp_interface")
+    bdmpInterfaceDf.createOrReplaceTempView("PgInterface")
+
+    val bdmpInterfaceFieldDf = getPostgresDf(spark, connOptionsMap, "app_upt.bdmp_interface_field")
+    bdmpInterfaceFieldDf.createOrReplaceTempView("PgField")
+
+    val bdmpInterfaceFileDf = getPostgresDf(spark, connOptionsMap, "app_upt.bdmp_interface_file")
+    bdmpInterfaceFileDf.createOrReplaceTempView("PgFile")
+
+    val bdmpSqlMapDf = getPostgresDf(spark, connOptionsMap, "app_upt.bdmp_sql_map")
+    bdmpSqlMapDf.createOrReplaceTempView("PgSqlMap")
+
+
+    val sql1 =
+      """select
+        |    bi.id as bi_id,
+        |    bif.id as bif_id,
+        |    bif.tab_comment_cn,
+        |    bif.load_solution,
+        |    bif.full_delta,
+        |    bif.if_physical_deletion,
+        |    lower(bi.source_system)||'_'||lower(coalesce(bif.file_alias,bif.name)) as ddl_name
+        |from PgFile bif
+        |         left join PgInterface bi on bi.id = bif.interface_id
+        |where bi.status=3 and bi.deleted=false and  bif.file_alias = '""".stripMargin + inputFileName + "';"
+
+    println(sql1)
+    spark.sql(sql1).show()
     spark.stop()
 
   }
+
+
+  def getPostgresDf(sparkSession: SparkSession, connOptionsMap: Map[String, String], tableName: String): DataFrame = {
+    sparkSession.read.format("jdbc")
+      .options(connOptionsMap)
+      .option("dbtable", tableName)
+      .load()
+  }
+
+
 }
